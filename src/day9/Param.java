05fd7b7096c418f7c50bc3ed9091b6b92d8e43d2
wcqaguxa <a.k.wolska@student.tudelft.nl>
2019-12-14 15:39:05 +0100

    day 9 part 1 and 2

---
+++ src/day9/Param.java
@@ -0,0 +1,32 @@
+package day9;
+
+class Param {
+    private int mode;
+    private int relativeBase;
+    private Instruction mother;
+
+    /**
+     * This is Param constructor
+     * @param mode - 0 if parameter mode, immediate mode otherwise
+     */
+     Param(int mode, int relativeBase, Instruction mother) {
+        this.mode = mode;
+        this.relativeBase = relativeBase;
+        this.mother = mother;
+     }
+
+    /**
+     * This is method to determine what index of paramter is retrueved, depending on mdoe
+     * @param i - pointer to given parameter
+     * @return - pointer to the value to be retrieved.
+     */
+    long execute(int i) {
+        if (mode==1) {
+            return i;
+        } else if (mode == 2) {
+            int value = mother.getValue(i)+relativeBase;
+            return value;
+        }
+        return mother.getValue(i);
+    }
+}
---
+++ test/day9/testPart1.java
@@ -0,0 +1,31 @@
+package day9;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class testPart1 {
+
+
+    @Test
+    public void test1() {
+        int[] intcode = { 1102,34915192,34915192,7,4,7,99,0};
+        long[] longcode = Arrays.stream(intcode).mapToLong(i -> i).toArray();
+
+        ResultTuple result = Util.solve(longcode, new LinkedList<>());
+        String res = Long.toString(result.get());
+        Assertions.assertEquals(res.length(), 16);
+    }
+
+    @Test
+    public void test2() {
+        int[] intcode = {109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99};
+        long[] longcode = Arrays.stream(intcode).mapToLong(i -> i).toArray();
+
+        ResultTuple result = Util.solve(longcode, new LinkedList<>());
+        long[] res = result.getArray();
+        Assertions.assertArrayEquals(longcode, res);
+    }
+}
